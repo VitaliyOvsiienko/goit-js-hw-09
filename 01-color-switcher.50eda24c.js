d2216c314c61711ad051fe8cf1ec1a3b88802eb1
VitaliyOvsiienko <106005736+VitaliyOvsiienko@users.noreply.github.com>
2023-03-19 09:56:29 +0000

    Deploying to gh-pages from @ VitaliyOvsiienko/goit-js-hw-09@9b2a518dc5d19594c1c6832c316628e960f180fc 🚀

---
+++ 01-color-switcher.50eda24c.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},n=0;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.50eda24c.js.map
