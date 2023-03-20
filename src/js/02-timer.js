2092dd3279aaaf5f119a7de103cb905807a87bb8
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-03-20 10:52:37 +0200

    task - 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// let stopTime = null;
+
+// const dateTimePicker = document.querySelector('#datetime-picker');
+// const startBtn = document.querySelector('[data-start]');
+// const daysElement = document.querySelector('[data-days]');
+// const hoursElement = document.querySelector('[data-hours]');
+// const minutesElement = document.querySelector('[data-minutes]');
+// const secondsElement = document.querySelector('[data-seconds]');
